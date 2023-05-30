98ecc287f5604c9a92d55f3e852651c1730a64ab
Patrick Mwaniki <patrickmwanikk@gmail.com>
2023-05-30 17:05:42 +0300

    view all tasks hover effect

--- wp-content/themes/jpj-system/page-viewalltasks.php
+++ wp-content/themes/jpj-system/page-viewalltasks.php
@@ -7,7 +7,7 @@
 ?>
 <?php get_header(); ?>
 <?php
-    $avatar = get_template_directory_uri() . '/custom/memoji-modified.png';
+$avatar = get_template_directory_uri() . '/custom/memoji-modified.png';
 ?>
 
 <section class="Dashboard">
@@ -37,19 +37,19 @@
             <a href="">All Employees</a>
         </div>
     </div>
-    <a href="http://localhost/jpj-system/clicked-project/">
 
-        <div class="Contents-Container">
-            <div class="Contents-header">
-                <div>
-                    <p>All Task</p>
-                </div>
-                <div>
+    <div class="Contents-Container">
+        <div class="Contents-header">
+            <div>
+                <p>All Task</p>
+            </div>
+            <div>
                 <i class="bi bi-laptop"></i>
                 <p>Status Tracker</p>
-                </div>
             </div>
-            <hr>
+        </div>
+        <hr>
+        <a href="http://localhost/jpj-system/clicked-project/">
             <div class="Contents">
                 <div class="profile-pic">
                     <div class="avatar">
@@ -59,7 +59,8 @@
                 </div>
                 <div class="project-info">
                     <p class="project-title">Sample Title</p>
-                    <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elite Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+                    <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elite Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit</p>
                 </div>
                 <div class="Status">
                     <div class="status-tracker">
@@ -71,7 +72,7 @@
                         </div>
                     </div>
                     <div>
-                        <div>
+                        <div class=" due-date">
                             <p>April 15, 2023</p>
                         </div>
                         <div class="rocket">
@@ -80,7 +81,55 @@
                         </div>
                     </div>
                 </div>
+                <div class="buttons">
+                    <div class="edit">
+                        <button><i class="bi bi-pencil-square"></i>Edit</button>
+                    </div>
+                    <div class="delete">
+                        <button><i class="bi bi-trash3-fill"></i>Delete</button>
+                    </div>
+                </div>
             </div>
-        </div>
+            <div class="Contents">
+                <div class="profile-pic">
+                    <div class="avatar">
+                        <img src="<?php echo $avatar ?>" alt="avatar">
+                    </div>
+                    <p class="employee-name">Employee Name</p>
+                </div>
+                <div class="project-info">
+                    <p class="project-title">Sample Title</p>
+                    <p class="project-description">Lorem ipsum dolor sit amet, consectetur adipiscing elite Lorem ipsum
+                        dolor sit amet, consectetur adipiscing elit</p>
+                </div>
+                <div class="Status">
+                    <div class="status-tracker">
+                        <div>
+                            <p>Due:</p>
+                        </div>
+                        <div>
+                            <p>Status:</p>
+                        </div>
+                    </div>
+                    <div>
+                        <div class=" due-date">
+                            <p>April 15, 2023</p>
+                        </div>
+                        <div class="rocket">
+                            <p>Launched</p>
+                            <i class="bi bi-rocket-fill"></i>
+                        </div>
+                    </div>
+                </div>
+                <div class="buttons">
+                    <div class="edit">
+                        <button><i class="bi bi-pencil-square"></i>Edit</button>
+                    </div>
+                    <div class="delete">
+                        <button><i class="bi bi-trash3-fill"></i>Delete</button>
+                    </div>
+                </div>
+            </div>
+    </div>
     </a>
 </section>
